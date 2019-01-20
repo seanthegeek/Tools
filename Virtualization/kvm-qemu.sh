805c6ab49d665127ccbdaa525907a2dc446f5868
Sean Whalen <whalenster@gmail.com>
2019-01-20 14:28:16 -0500

    Remove existing virt-manager

--- Virtualization/kvm-qemu.sh
+++ Virtualization/kvm-qemu.sh
@@ -170,7 +170,8 @@ function install_kvm_linux_apt() {
     # WSL support
     apt-get install gcc make gnutls-bin -y
     # remove old
-    apt-get purge libvirt0 libvirt-bin -y
+    apt-get purge libvirt0 libvirt-bin virt-manager -y
+    apt-get-autoremove -y
     install_libvirt
 
     systemctl enable libvirtd.service
